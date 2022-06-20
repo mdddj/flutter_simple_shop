14ce5bbd0bf48f5912eb46f19c1dc19765abfeb6
mddd <413153189@qq.com>
2022-06-20 23:47:50 +0800

    修复一处找不到包的错误

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import ali_bc_taoke_sdk
 import package_info
 import path_provider_macos
 import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AliBcTaokeSdkPlugin.register(with: registry.registrar(forPlugin: "AliBcTaokeSdkPlugin"))
   FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
