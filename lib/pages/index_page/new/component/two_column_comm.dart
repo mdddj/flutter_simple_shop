fe28979a9c9a38a409bd084c30bccadd13820219
梁典典 <413153189@qq.com>
2022-07-25 09:14:03 +0800

    修复首页一处小问题

--- lib/pages/index_page/new/component/two_column_comm.dart
+++ lib/pages/index_page/new/component/two_column_comm.dart
@@ -6,8 +6,8 @@ import 'package:provider/provider.dart';
 import '../../../best_seller_list.dart';
 import '../index_riverpod.dart';
 
-
-typedef IndexTwoColumnImageWidgetBuild = ExtendedImage Function(double w, double h);
+typedef IndexTwoColumnImageWidgetBuild = ExtendedImage Function(
+    double w, double h);
 
 class IndexColumnWidget extends StatelessWidget {
   const IndexColumnWidget({Key? key}) : super(key: key);
@@ -15,15 +15,41 @@ class IndexColumnWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final products = context.read<IndexState>().hotDayProducts;
+    debugPrint("产品数据:${products.length}");
     return SliverPadding(
-      sliver: SliverWaterfallFlow.count(crossAxisCount: 2,mainAxisSpacing: 12,crossAxisSpacing: 12,children: [
-        TwoColumnCommWidget(imageBuilder: (double w, double h) {
-          return ExtendedImage.network(products[0].mainPic!,width: w,height: h,borderRadius: BorderRadius.circular(12),shape: BoxShape.rectangle,);
-        }, title: '畅销榜单', subTitle: '所有人,买它',onTap: BestSellerListPage.nav),
-        TwoColumnCommWidget(imageBuilder: (double w, double h) {
-          return ExtendedImage.network(products[1].mainPic!,width: w,height: h,borderRadius: BorderRadius.circular(12),shape: BoxShape.rectangle);
-        }, subTitle: '新鲜更有趣', title: '每日上新',)
-      ],), padding: const EdgeInsets.symmetric(horizontal: 12),
+      sliver: SliverWaterfallFlow.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 12,
+        crossAxisSpacing: 12,
+        children: [
+          TwoColumnCommWidget(
+              imageBuilder: (double w, double h) {
+                return ExtendedImage.network(
+                  products[0].mainPic!,
+                  width: w,
+                  height: h,
+                  borderRadius: BorderRadius.circular(12),
+                  shape: BoxShape.rectangle,
+                );
+              },
+              title: '畅销榜单',
+              subTitle: '所有人,买它',
+              onTap: BestSellerListPage.nav),
+          if (products.length > 1)
+            TwoColumnCommWidget(
+              imageBuilder: (double w, double h) {
+                return ExtendedImage.network(products[1].mainPic!,
+                    width: w,
+                    height: h,
+                    borderRadius: BorderRadius.circular(12),
+                    shape: BoxShape.rectangle);
+              },
+              subTitle: '新鲜更有趣',
+              title: '每日上新',
+            )
+        ],
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 12),
     );
   }
 }
@@ -34,35 +60,51 @@ class TwoColumnCommWidget extends StatelessWidget {
   final String title;
   final String subTitle;
   final VoidCallback? onTap;
-  const TwoColumnCommWidget({Key? key,required this.imageBuilder, required this.title, required this.subTitle, this.onTap}) : super(key: key);
+
+  const TwoColumnCommWidget(
+      {Key? key,
+      required this.imageBuilder,
+      required this.title,
+      required this.subTitle,
+      this.onTap})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints) {
-      return GestureDetector(
-        onTap: onTap,
-        child: Container(
-          padding: const EdgeInsets.all(12),
-          width: constraints.maxWidth,
-          decoration: BoxDecoration(color: Colors.grey.shade200,
-          borderRadius: BorderRadius.circular(12)
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return GestureDetector(
+          onTap: onTap,
+          child: Container(
+            padding: const EdgeInsets.all(12),
+            width: constraints.maxWidth,
+            decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                borderRadius: BorderRadius.circular(12)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    Text(subTitle,
+                        style: TextStyle(
+                            fontSize: 13, color: Colors.grey.shade400))
+                  ],
+                )),
+                imageBuilder.call(
+                    constraints.maxWidth * .3, constraints.maxWidth * .3)
+              ],
+            ),
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Expanded(child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children:  [
-                   Text(title,style: const TextStyle(fontSize: 18),),
-                  Text(subTitle,style: TextStyle(fontSize: 13,color: Colors.grey.shade400))
-                ],
-              )),
-              imageBuilder.call(constraints.maxWidth * .3,constraints.maxWidth * .3)
-            ],
-          ),
-        ),
-      );
-    },);
+        );
+      },
+    );
   }
 }
